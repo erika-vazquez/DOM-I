368685b635079fe197e760a8dc7cc44cb3e0d285
Erika <ito2016@protonmail.ch>
2019-06-17 14:46:05 -0500

    Added the main content's section Text

--- js/index.js
+++ js/index.js
@@ -59,5 +59,21 @@ aTags[4].innerText = "About"
 aTags[5].setAttribute('class', '.nav-item-6')
 aTags[5].innerText = "Product"
 
-//changed the nav text color to green
+//changes the navigation text color to green
 aTags.forEach(link => link.style.color = 'green')
+
+//Two new items created for the navigation system
+
+//creates the main content section
+let mainContent = document.querySelectorAll('.text-content');
+
+mainContent[0].querySelector('h4').textContent = siteContent['main-content']['features-h4']
+mainContent[0].querySelector('p').textContent = siteContent['main-content']['features-content']
+mainContent[1].querySelector('h4').textContent = siteContent['main-content']['about-h4']
+mainContent[1].querySelector('p').textContent = siteContent['main-content']['about-content']
+mainContent[2].querySelector('h4').textContent = siteContent['main-content']['services-h4']
+mainContent[2].querySelector('p').textContent = siteContent['main-content']['services-content']
+mainContent[3].querySelector('h4').textContent = siteContent['main-content']['product-h4']
+mainContent[3].querySelector('p').textContent = siteContent['main-content']['product-content']
+mainContent[4].querySelector('h4').textContent = siteContent['main-content']['vision-h4']
+mainContent[4].querySelector('p').textContent = siteContent['main-content']['vision-content']
